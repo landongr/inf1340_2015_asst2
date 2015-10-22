4570c6ccab972635976726dd5818e5ba665cfb12
landongr <graham.cilandon@gmail.com>
2015-10-22 11:15:40 -0400

    First Attempt at #1

--- exercise1.py
+++ exercise1.py
@@ -11,8 +11,19 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
+def user_input():
+    """
+    This function checks to make sure user input is legal
+    and returns legal user input to the pig latin function
+    """
+    word = raw_input("Enter your word!")
+    while word.isalpha() == False:
+        print "Please enter a single word"
+        word = raw_input("Enter your word!")
+    else:
+        return word.lower()
 
-def pig_latinify(word):
+def pig_latinify():
     """
     Describe your function
 
@@ -21,6 +32,21 @@ def pig_latinify(word):
     :raises:
 
     """
-    result = ""
+    vowels = ["a","e","i","o","u","y"]
+    word = user_input()
 
-    return result
+    while True:
+        for letter in vowels:
+            if word[0] == letter:
+                print word + "yay"
+                return False
+        else:
+            for letter in vowels:
+                for let in word:
+                    if letter == let:
+                        i = word.index(let)
+                        print word[i:]+word[:i]+"ay"
+                        return False
+
+pig_latinify()
+
